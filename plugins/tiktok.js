aa9931c084c9f76b0cd3a832e74f978c375d4329
bebab0101 <96487027+bebab0101@users.noreply.github.com>
2021-12-21 22:47:35 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,16 +1,22 @@
 let fetch = require('node-fetch')
-
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(API('Velgrynd', '/api/tiktok', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw await res.text()
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
   let json = await res.json()
-  conn.sendFile(m.chat, json.result.url_nowm, 'tiktok.mp4', json.result.desc, m)
+  if (!json.status) throw json
+  try {
+    await conn.sendFile(m.chat, json.server_1, 'tiktok.mp4', '', m)
+  } catch (e) {
+    m.reply('Server 1 Failed, Retrying with Server 2')
+    await conn.sendFile(m.chat, json.server_2, 'tiktok.mp4', '', m)
+  }
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(dl)?)$/i
 
-handler.limit = true
+handler.command = /^(tiktok(dl)?)$/i
 
 module.exports = handler
